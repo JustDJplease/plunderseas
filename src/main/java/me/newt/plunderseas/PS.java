9209844e644ae3d0bbe1370a5b566ec011e5363d
TheBlockBender <floris.jolink@gmail.com>
2020-07-30 16:28:19 +0200

    init commit

---
+++ src/main/java/me/newt/plunderseas/PS.java
@@ -0,0 +1,17 @@
+package me.newt.plunderseas;
+
+import me.newt.plunderseas.plundering.UtilCreateOceanWorld;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PS extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Creating world...");
+        System.out.println("Created: " + UtilCreateOceanWorld.createWorld().getName());
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/me/newt/plunderseas/plundering/UtilCreateOceanWorld.java
@@ -0,0 +1,21 @@
+package me.newt.plunderseas.plundering;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
+
+import java.util.UUID;
+
+public class UtilCreateOceanWorld {
+
+    public static World createWorld() {
+        UUID uuid = UUID.randomUUID();
+        WorldCreator worldCreator = new WorldCreator("ocean_" + uuid);
+        worldCreator.environment(World.Environment.NORMAL);
+        worldCreator.generateStructures(false);
+        worldCreator.type(WorldType.FLAT);
+        worldCreator.generatorSettings("{\"structures\": {\"structures\": {}}, \"layers\": [{\"block\": \"bedrock\", \"height\": 1}, {\"block\": \"sand\", \"height\": 25}, {\"block\": \"water\", \"height\": 36}], \"biome\":\"deep_ocean\"}");
+        return Bukkit.createWorld(worldCreator);
+    }
+}
