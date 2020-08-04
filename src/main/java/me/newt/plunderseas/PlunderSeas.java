c3ca5222814dc2b21f43fa07f15f77c1b77175fc
TheBlockBender <floris.jolink@gmail.com>
2020-08-04 11:30:39 +0200

    Updated to-do list

--- src/main/java/me/newt/plunderseas/PlunderSeas.java
+++ src/main/java/me/newt/plunderseas/PlunderSeas.java
@@ -18,7 +18,52 @@ public class PlunderSeas extends JavaPlugin {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
     //                                    TASKLIST                                       //
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
-    // - Display soulpoints, money and skill-rating in action bar.
+    // Currency:
+    // - Hook into vault.
+    //
+    // Adventuring & Shipwrecks
+    // - Shipwreck chests regenerate.
+    // - Looting yields cash.
+    //
+    // Raiding:
+    // - Create blank world upon enabling.
+    // - Trash blank world upon disabling.
+    // - Hook into precious-stones.
+    // - Copy raided islands to that world (0,0).
+    // - Setup world-border.
+    // - One raid active at a time.
+    // - Warmup in which defending players can decide to participate.
+    // - Teleport participating players.
+    // - Keep track of players near trap armor-stands.
+    // - Destruction of non-default-generated blocks leads to income.
+    // - Destruction of special blocks leads to extra income.
+    // - Money is lost from defending team.
+    // - Limited time to raid.
+    // - Cool-down after raid.
+    // - Skill rating for victory & loss.
+    // - Death = out of the raid
+    // - No remaining attackers or time over = end of the raid. (Check every second, account for leavers).
+    //
+    // Skill-rating:
+    // - Crew with the highest skill-rating gets a weekly & monthly reward.
+    //
+    // Traps:
+    // - Popular trap plugin to hook into?
+    // -    Potion effect traps.
+    // -    Launching traps.
+    // -    MythicMob traps.
+    // -    Disappearing floor (3x3) traps.
+    // - Players can place and remove traps (armor-stands).
+    // - Trap (items) can be bought.
+    // - Animated armor-stands.
+    // - Upon raid, these are hidden.
+    //
+    // Display:
+    // - Souls, money and skill-rating in action bar.
+    //
+    // Souls:
+    // - /soulpoint(s) set <player> <number or max>.
+    // - /soulpoint(s) get <player>.
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
     //                                    ENABLING                                       //
